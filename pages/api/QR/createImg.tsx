9273be7d3d8b7d1b5f58d0fa502b3368b4625abe
DanielKnourek <dev.danielknourek@gmail.com>
2021-07-06 02:17:22 +0200

    feature ImgQR component

--- pages/api/QR/createImg.tsx
+++ pages/api/QR/createImg.tsx
@@ -1,25 +1,36 @@
-import { error } from "console";
 import { NextApiRequest, NextApiResponse } from "next";
 
 import QRgenerator from 'qrcode'
 
-async function createImg(message: string): Promise<string | boolean> {
+class ImageCreationError extends Error{
+  constructor(...params: any[]){
+    super(...params)
+    if ( Error.captureStackTrace){
+      Error.captureStackTrace(this, ImageCreationError)
+    }
+    const cause: string = "Could not create image";
+  }
+}
+
+export async function createImg(message: string): Promise<string> {
   return await QRgenerator.toDataURL(message)
   .then(imageData => {
     return imageData.toString();
   })
   .catch(err => {
-    return false;
+    throw new ImageCreationError();
   })
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    console.log(req);
     if( !req.query.hasOwnProperty('message')){
       return res.status(400).send("Missing [message] parameter!");
     }
-    let imageData: string | boolean = await createImg(req.query.message.toString());
-    if( !imageData ){
+
+    let imageData: string = "";
+    try{
+      imageData = await createImg(req.query.message.toString());
+    }catch(err: any){
       return res.status(404).send("Unable to create image");
     }
     res.status(200).json({success: true, image: imageData})
--- pages/code/index.tsx
+++ pages/code/index.tsx
@@ -1,48 +1,15 @@
 import QRCode from 'qrcode'
 import { ReactFragment, useRef, useState } from 'react'
 
+import {ImgQR} from '../../components/ImgQR'
+
 function Page_code(): ReactFragment{
     // https://www.npmjs.com/package/qrcode
-
-    const [qrCodePicture, setQrCode] = useState("");
-    
-    const canvasRef = useRef(null)
-    let qrCanvas = <canvas ref={canvasRef}></canvas>
-    const canvasObj: any = canvasRef.current;
-    // console.log(canvasObj.getContext('2d'))
-    let ctx = canvasObj.getContext('2d')
-    const imgQR = new Image()
-    imgQR.src = qrCodePicture;
-    ctx.drawImage(imgQR, 0,0)
-    const getCanvasRenderingContext2D = (canvas: HTMLCanvasElement): CanvasRenderingContext2D => {
-      const context = canvas.getContext('2d')
-  
-      if (context === null) {
-          throw new Error('This browser does not support 2-dimensional canvas rendering contexts.');
-      }
-  
-      return context;
-  }
-    // qrCanvas.drawImage()
-
-    QRCode.toDataURL('I am a pony!')
-    .then(url => {
-      // console.log(url)
-      setQrCode(url)
-      // console.log(canvasObj)
-      // canvasObj.drawImage(url, 0,0)
-      
-    })
-    .catch(err => {
-      console.error(err)
-    })
     
 
     return(<>
-    <script src="http://localhost:8097"></script>
         <h1>Toto je můj kód</h1>
-        {qrCanvas}
-        <p>{qrCodePicture}</p>
+        <ImgQR message="Hello daniel22" width={500} height={500}/>
     </>)
 }
 
